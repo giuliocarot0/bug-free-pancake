d1680747f84e1a087f6fa58c959b7fd0429aca17
Giulio Carota <giulio.carota@outlook.it>
2019-05-09 15:15:01 +0200

    Create CheckChar.asm
    Read an ascii value and checks if it's a number or not

---
+++ CheckChar.asm
@@ -0,0 +1,40 @@
+.data
+	msg1: .asciiz "Inserire val numerico: "
+	msg2: .asciiz "\n Stop"
+	msg3: .asciiz " -> Valid Char \n"
+	msg4: .asciiz " -> Invalid Char \n"
+
+	
+	.text
+	.globl main
+	
+main:
+
+	li $v0, 4
+	la $a0, msg1
+	syscall
+	
+cyc: 
+	li $v0, 12
+	syscall
+	beq $v0, 10, exit
+	bgt $v0 '9' NaN
+	blt $v0 '0' NaN
+	li $v0, 4
+	la $a0, msg3
+	syscall
+	j cyc
+
+NaN:
+	li $v0, 4
+	la $a0, msg4
+	syscall
+	j cyc
+	
+exit:	
+	li $v0, 4
+	la $a0, msg2
+	syscall
+
+.end main
+	
